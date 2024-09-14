47eb9cddb8a7136825e121cd3d8e83528fe27a30
marie-bisbo <49270534+marie-bisbo@users.noreply.github.com>
2024-09-14 12:21:13 +0100

    Restructure code into structs and functions

--- sdlproject/main.c
+++ sdlproject/main.c
@@ -6,163 +6,186 @@
 const int SCREEN_WIDTH = 1280;
 const int SCREEN_HEIGHT = 720;
 
-int main(int argc, char* args[])
+typedef struct _Entity
 {
-	SDL_Window* window = NULL;
+	int xPosition;
+	int yPosition;
+	SDL_Texture* texture;
+} Entity;
 
-	SDL_Surface* screenSurface = NULL;
+typedef struct _App
+{
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+} App;
+
+App* InitApp()
+{
+	App* app = malloc(sizeof(App));
+	if (NULL == app)
+	{
+		printf("Failed to allocate memory for app\n");
+
+		return NULL;
+	}
+
+	bool initFailed = false;
+
+	SDL_Window* window = NULL;
+	SDL_Renderer* renderer = NULL;
 
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
 		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+
+		initFailed = true;
 	}
 	else
 	{
 		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window == NULL)
+		if (NULL == window)
 		{
 			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+
+			initFailed = true;
 		}
-		else
+
+
+		renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+		if (NULL == renderer)
 		{
-			SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+			printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
 
-			if (!IMG_Init(IMG_INIT_PNG))
-			{
-				printf("FAIL!");
-
-				return 0;
-			}
-
-			screenSurface = SDL_GetWindowSurface(window);
-
-			//Fill the surface white
-			// SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xAF, 0xAF, 0xAF));
-
-			//Update the surface
-			// SDL_UpdateWindowSurface(window);
-
-			SDL_Texture* playerTexture = IMG_LoadTexture(renderer, "Assets/character_big.png");
-			if (playerTexture == NULL)
-			{
-				printf("NULL PLAYER TEXTURE");
-			}
-
-			SDL_Rect position = {100, 100, 64, 64};
-
-			SDL_Event event;
-			bool quit = false;
-
-			while (quit == false)
-			{
-				SDL_SetRenderDrawColor(renderer, 0, 204, 0, 255);
-				SDL_RenderClear(renderer);
-
-				SDL_Rect size;
-				size.w = 64;
-				size.h = 64;
-
-
-				/*
-				if (SDL_RenderCopy(renderer, playerTexture, NULL, &position) < 0)
-				{
-					printf("ERROR: %s", SDL_GetError());
-				}
-				SDL_RenderPresent(renderer);
-				*/
-
-				while (SDL_PollEvent(&event)) 
-				{ 
-					if (event.type == SDL_QUIT)
-					{
-						quit = true;
-					}
-					else if (event.type == SDL_KEYDOWN)
-					{
-						switch (event.key.keysym.sym)
-						{
-						case SDLK_UP:
-							printf("UP\n");
-							position.y -= 5;
-
-							if (SDL_QueryTexture(playerTexture, NULL, NULL, &size.w, &size.h) < 0)
-							{
-								printf("ERROR: %s", SDL_GetError());
-							}
-
-							if (SDL_RenderCopy(renderer, playerTexture, NULL, &position) < 0)
-							{
-								printf("ERROR: %s", SDL_GetError());
-							}
-							SDL_RenderPresent(renderer);
-							break;
-						case SDLK_DOWN:
-							printf("DOWN\n");
-							position.y += 5;
-
-							if (SDL_QueryTexture(playerTexture, NULL, NULL, &size.w, &size.h) < 0)
-							{
-								printf("ERROR: %s", SDL_GetError());
-							}
-							if (SDL_RenderCopy(renderer, playerTexture, NULL, &position) < 0)
-							{
-								printf("ERROR: %s", SDL_GetError());
-							}
-							SDL_RenderPresent(renderer);
-							break;
-						case SDLK_LEFT:
-							printf("LEFT\n");
-							position.x -= 5;
-
-							if (SDL_QueryTexture(playerTexture, NULL, NULL, &size.w, &size.h) < 0)
-							{
-								printf("ERROR: %s", SDL_GetError());
-							}
-							if (SDL_RenderCopy(renderer, playerTexture, NULL, &position) < 0)
-							{
-								printf("ERROR: %s", SDL_GetError());
-							}
-							SDL_RenderPresent(renderer);
-							break;
-						case SDLK_RIGHT:
-							printf("RIGHT\n");
-							position.x += 5;
-
-							if (SDL_QueryTexture(playerTexture, NULL, NULL, &size.w, &size.h) < 0)
-							{
-								printf("ERROR: %s", SDL_GetError());
-							}
-							if (SDL_RenderCopy(renderer, playerTexture, NULL, &position) < 0)
-							{
-								printf("ERROR: %s", SDL_GetError());
-							}
-							SDL_RenderPresent(renderer);
-							break;
-						default:
-							printf("You pressed a different key");
-							break;
-						}
-					}
-					else
-					{
-						if (SDL_QueryTexture(playerTexture, NULL, NULL, &size.w, &size.h) < 0)
-						{
-							printf("ERROR: %s", SDL_GetError());
-						}
-						if (SDL_RenderCopy(renderer, playerTexture, NULL, &position) < 0)
-						{
-							printf("ERROR: %s", SDL_GetError());
-						}
-						SDL_RenderPresent(renderer);
-					}
-				} 
-				
-				SDL_Delay(16);
-			}
+			initFailed = true;
 		}
+	}	
+
+	if (initFailed)
+	{
+		free(app);
+		app = NULL;
+
+		return NULL;
 	}
 
-	SDL_DestroyWindow(window);
+	app->window = window;
+	app->renderer = renderer;
+
+	return app;
+}
+
+int main(int argc, char* args[])
+{
+	App* app = InitApp();
+	if (NULL == app)
+	{
+		printf("Failed to initialise app\n");
+
+		return 0;
+	}
+
+	Entity* entity = malloc(sizeof(Entity));
+	if (NULL == entity)
+	{
+		printf("Failed to allocate memory for entity\n");
+
+		return 0;
+	}
+
+	if (!IMG_Init(IMG_INIT_PNG))
+	{
+		printf("Failed to initialise PNG\n");
+
+		return 0;
+	}
+
+	SDL_Texture* playerTexture = IMG_LoadTexture(app->renderer, "Assets/character_big.png");
+	if (NULL == playerTexture)
+	{
+		printf("NULL PLAYER TEXTURE\n");
+	}
+
+	entity->xPosition = 100;
+	entity->yPosition = 100;
+	entity->texture = playerTexture;
+
+	SDL_Rect position = {entity->xPosition, entity->yPosition, 64, 64};
+
+	SDL_Event event;
+	bool quit = false;
+
+	while (false == quit)
+	{
+		SDL_SetRenderDrawColor(app->renderer, 0, 204, 0, 255);
+		SDL_RenderClear(app->renderer);
+
+		SDL_Rect size;
+		size.w = 64;
+		size.h = 64;
+
+		while (SDL_PollEvent(&event)) 
+		{ 
+			if (event.type == SDL_QUIT)
+			{
+				quit = true;
+			}
+			else if (event.type == SDL_KEYDOWN)
+			{
+				switch (event.key.keysym.sym)
+				{
+				case SDLK_UP:
+					printf("UP\n");
+					position.y -= 5;
+					break;
+				case SDLK_DOWN:
+					printf("DOWN\n");
+					position.y += 5;
+					break;
+				case SDLK_LEFT:
+					printf("LEFT\n");
+					position.x -= 5;
+					break;
+				case SDLK_RIGHT:
+					printf("RIGHT\n");
+					position.x += 5;
+					break;
+				default:
+					printf("You pressed a different key\n");
+					break;
+				}
+
+				entity->xPosition = position.x;
+				entity->yPosition = position.y;
+
+				if (SDL_QueryTexture(playerTexture, NULL, NULL, &size.w, &size.h) < 0)
+				{
+					printf("ERROR: %s\n", SDL_GetError());
+				}
+
+				if (SDL_RenderCopy(app->renderer, playerTexture, NULL, &position) < 0)
+				{
+					printf("ERROR: %s\n", SDL_GetError());
+				}
+				SDL_RenderPresent(app->renderer);
+			}
+			else
+			{
+				if (SDL_QueryTexture(playerTexture, NULL, NULL, &size.w, &size.h) < 0)
+				{
+					printf("ERROR: %s\n", SDL_GetError());
+				}
+				if (SDL_RenderCopy(app->renderer, playerTexture, NULL, &position) < 0)
+				{
+					printf("ERROR: %s\n", SDL_GetError());
+				}
+				SDL_RenderPresent(app->renderer);
+			}
+		} 
+		
+		SDL_Delay(16);
+	}
+
+	SDL_DestroyWindow(app->window);
 
 	SDL_Quit();
 
